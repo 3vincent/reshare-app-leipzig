b77642da0d9ce2c31b908f4e09f0ace6bc9f81d4
vincent <vincent@grrlz.net>
2021-05-26 17:03:09 +0200

    put uuid function into separate file

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 const Person = require('./src/person.js')
 
 
---
+++ src/uuid-func.js
@@ -0,0 +1,9 @@
+// Stolen from Stackoverflow
+// https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid
+//
+module.exports = function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+};
