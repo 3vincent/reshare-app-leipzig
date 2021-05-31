8577137c0978dbf8f068f0dde792b7ec78fe5655
Ben Sukstorf <bsukstorf@gmail.com>
2021-05-31 10:29:13 +0200

    add person, add offers for testing

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 const Person = require('./person')
 
 const ben = new Person('Ben', 'ben@someemail.com', 29, '', ['Leipzig', '04277'])
-const armagan = new Person('Armagan', 'armagan@someemail.com', 29, '', ['Berlin', '11231'])
+const armagan = new Person('Armagan', 'armagan@someemail.com', 29, '', ['Berlin', '11234'])
 
 const bensOffer = ben.createOffer('My old pillbox hat', ['Leipzig', '04277'], 'Clothing')
 const bensOffer2 = ben.createOffer('Wash dishes', ['Leipzig', '04277'], 'Services')
 
-armagan.createOffer('Dish Washer', ['Berlin', '11231'], 'Electrical Devices')
+const armaganOffer = armagan.createOffer('Dish Washer', ['Berlin', '11234'], 'Electrical Devices')
 armagan.likeOffer(bensOffer)
 armagan.likeOffer(bensOffer2)
 
 ben.likeOffer(bensOffer)
+ben.likeOffer(armaganOffer)
 
 console.log(ben)
 console.log(armagan)
 
+console.log(armagan.profile)
 console.log(ben.profile)
 
 // console.log('Bens Offers Liked by output:', ben.offers[0].likedBy)
