86d9816ffc8d745fb2cadcba1bc68037a044d622
vincent <vincent@grrlz.net>
2021-06-01 21:00:28 +0200

    Add timestamp to comment class

--- src/comments.js
+++ src/comments.js
@@ -2,6 +2,7 @@ module.exports = class Comment {
   constructor(offer, sender, comment) {
     this.offerUUID = offer.offerUUID
     this.sender = sender.name
+    this.creationTime = Date.now()
     this.comment = comment
   }
 }
