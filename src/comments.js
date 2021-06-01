1db79509668dd588054a45119b134783307d3c73
vincent <vincent@grrlz.net>
2021-06-01 21:00:09 +0200

    Add fake-UUID to comment function for testing

--- src/comments.js
+++ src/comments.js
@@ -1,6 +1,6 @@
 module.exports = class Comment {
   constructor(offer, sender, comment) {
-    this.offer = offer.offerUUID
+    this.offerUUID = offer.offerUUID
     this.sender = sender.name
     this.comment = comment
   }
