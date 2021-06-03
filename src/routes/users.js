5d4688be63049f71ae40d94b5c0ee51e344a5b69
Ben Sukstorf <bsukstorf@gmail.com>
2021-06-03 22:06:00 +0200

    Add Express.js, restructure files in directories

---
+++ src/routes/users.js
@@ -0,0 +1,31 @@
+const { query } = require('express')
+const express = require('express')
+
+const router = express.Router()
+
+const Person = require('../models/person')
+
+const ben = new Person('ben', 'ben@someemail.com', 29, '', ['Leipzig', '04277'])
+const armagan = new Person('armagan', 'armagan@someemail.com', 29, '', ['Berlin', '11234'])
+
+const users = [ben, armagan]
+
+/* GET home page. */
+
+/* GET users listing. */
+router.get('/', (req, res, next) => {
+  let result = users
+
+  if (req.query.name) {
+    result = users.find(user => user.name === req.query.name)
+  }
+  res.send(result)
+})
+
+router.get('/:userID', (req, res, next) => {
+  const user = users[req.params.userID]
+  if (user) res.send(user)
+  else res.sendStatus(404)
+})
+
+module.exports = router
