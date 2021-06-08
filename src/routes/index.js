d6054a06e2922e48d47765343a16fcdeb63aa95c
Ben Sukstorf <bsukstorf@gmail.com>
2021-06-08 09:58:53 +0200

    Take 'next' from router parameters

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,8 @@ const express = require('express')
 const router = express.Router()
 
 const { offers } = require('../models/db')
-router.get('/', (req, res, next) => {
+
+router.get('/', (req, res) => {
   res.render('index', { title: 'Leipzig Share App' })
 })
 
