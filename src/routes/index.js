962f421ba77d342ca4a035613cb8f353ddb2cd00
Ben Sukstorf <bsukstorf@gmail.com>
2021-06-08 09:59:18 +0200

    Comment out router Example for Object return

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,11 @@ const { offers } = require('../models/db')
 
 router.get('/', (req, res) => {
   res.render('index', { title: 'Leipzig Share App' })
+
+  // return as object EXAMPLE:
+  //  res.send({
+  //   message: 'hello',
+  // })
 })
 
 module.exports = router
