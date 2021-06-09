83156b682117c27f4bc063a0689ed1f446bc6cff
Ben Sukstorf <bsukstorf@gmail.com>
2021-06-09 15:09:12 +0200

    Delete old Tip Calculator Example from Project

--- _old-tip-calculator/index.js
+++
@@ -1,9 +0,0 @@
-const button = document.getElementById("calculateButton")
-button.addEventListener("click", calculate);
-
-function calculate() {
-    const amount = document.getElementById("totalAmount").value
-    const tipAmount = amount * 0.1
-    document.getElementById("result").innerHTML = "Amount of tip should be paid: " + tipAmount
-}
-
