677843efbfaf739a0ba513dd0fe5d03bcd0b5cc4
Ben Sukstorf <bsukstorf@gmail.com>
2021-06-10 20:04:40 +0200

    Copy more test-cases to initialize function from old db.js

--- src/db.js
+++ src/db.js
@@ -1,24 +1,12 @@
-const uuidv4 = require('./lib/uuid-func')
-const Person = require('./models/person')
-const Offer = require('./models/offer')
-const Comment = require('./models/comment')
+// const uuidv4 = require('./lib/uuid-func')
+// const Person = require('./models/person')
+// const Offer = require('./models/offer')
+// const Comment = require('./models/comment')
 
-const persons = []
-const offers = []
+// const persons = []
+// const offers = []
 
 async function main() {
-  // const bensOffer2 = await Offer.create({
-  //   title: 'Wash dishes',
-  //   offerUUID: uuidv4,
-  //   location: ['Leipzig', '04277'],
-  //   category: 'Services',
-  //   description: 'My job is to do the dishes. I can help you clean if you need help',
-  // })
-  // await armagan.likeOffer(bensOffer2)
-  // const commentb21 = await Comment.create({ creationTime: Date.now, comment: 'Awesome' })
-  // const commentb22 = await Comment.create({ creationTime: Date.now, comment: 'test test test' })
-  // await ben.leaveComment(commentb21)
-  // await armagan.leaveComment(commentb22)
   // const armaganOffer = await Offer.create({
   //   title: 'Dish Washer',
   //   offerUUID: uuidv4,
@@ -38,5 +26,5 @@ async function main() {
 
 main()
 
-exports.persons = persons
-exports.offers = offers
+// exports.persons = persons
+// exports.offers = offers
