bf76b704acd449d5f78c30caef89b7ff1d1b3fa9
Ben Sukstorf <bsukstorf@gmail.com>
2021-06-10 13:36:23 +0200

    Transform Person Class to mongoose Schema

--- src/models/person.js
+++ src/models/person.js
@@ -1,68 +1,74 @@
+const mongoose = require('mongoose')
+const autopopulate = require('mongoose-autopopulate')
 
 const Offer = require('./offer')
-
 const Comment = require('./comment')
 
-module.exports = class Person {
-  constructor(name, emailAddress, age, profilePhoto, location) {
-    this.name = name
-    this.emailAddress = emailAddress
-    this.age = age
-    this.profilePhoto = profilePhoto
-    this.location = location
-    this.savedOffers = []
-    this.offers = []
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  eMailAddress: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  profilePhoto: {
+    type: String,
+  },
+  location: [],
+  savedOffers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Offer',
+      autopopulate: true,
+    },
+  ],
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Offer',
+    },
+  ],
+
+  offers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Offer',
+      autopopulate: true,
+    },
+  ],
+})
+
+class Person {
+  async createOffer(offer) {
+    this.offers.push(offer)
+    await this.save()
+    return offer
   }
 
-  createOffer(title, location, category, descripton) {
-    const newOffer = new Offer(title, location, category, descripton)
-    this.offers.push(newOffer)
-    return newOffer
+  async likeOffer(offer) {
+    this.likes.push(offer)
+    offer.likedBy.push(this)
+
+    await offer.save()
+    await this.save()
   }
 
-  likeOffer(offer) {
-    offer.likedBy.push(this.name)
-  }
-
-  leaveComment(offer, comment) {
-    const newComment = new Comment(offer, this, comment)
-    offer.comments.push(newComment)
-    return newComment
-  }
-
-  get profile() {
-    return `
-
-# ${logSymbols.info} ${this.name}'s Profile (${this.age})
-    ${this.name} lives in ${this.location
-      .slice()
-      .reverse()
-      .map(area => area)
-      .join(' ')}
-
-### ${this.name} has 
-    ${this.offers.length} Offers
-
-### Offers (${this.offers.length})
-    ${this.offers
-      .map(
-        (offer, index) => `
-    ${index + 1}. ${offer.title}
-        Category: ${offer.category}
-        Location: ${offer.location
-          .slice()
-          .reverse()
-          .map(x => x)
-          .join(' ')}
-        => Status: ${offer.status}
-        => Liked by ${offer.likedBy.map(person => person).join(', ')} (${offer.likedBy.length})
-        => Commenters: ${offer.commenters
-          .slice()
-          .sort()
-          .filter((commenter, index2, array) => array.indexOf(commenter) === index2)
-          .join(', ')}
-        => Total Comments: ${offer.commenters.length}`
-      )
-      .join('\n')}`
-  }
+  // async leaveComment(offer, comment) {
+  //   const newComment = new Comment(offer, this, comment)
+  //   offer.comments.push(newComment)
+  //   return newComment
+  // }
 }
+
+personSchema.loadClass(Person)
+personSchema.plugin(autopopulate)
+
+module.exports = mongoose.model('Person', personSchema)
