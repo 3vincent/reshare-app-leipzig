00116178ef0d17526ecec0ae7500d098ab36eee9
Ben Sukstorf <bsukstorf@gmail.com>
2021-06-14 19:15:44 +0200

    Update database connection to use connectionString

--- src/database-connection.js
+++ src/database-connection.js
@@ -3,11 +3,15 @@ const mongoose = require('mongoose')
 const username = process.env.MONGODB_USERNAME
 const password = process.env.MONGODB_PASSWORD
 const dbName = process.env.MONGODB_DATABASE
+let connectionString = process.env.MONGODB_CONNECTION_STRING
 
+if (!connectionString) {
+  connectionString = `mongodb+srv://${username}:${password}@cluster0.zr7f6.mongodb.net/${dbName}?retryWrites=true&w=majority`
+}
 mongoose.set('debug', true)
 
 mongoose
-  .connect(`mongodb+srv://${username}:${password}@cluster0.zr7f6.mongodb.net/${dbName}?retryWrites=true&w=majority`, {
+  .connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
