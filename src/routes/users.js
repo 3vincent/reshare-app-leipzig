4f420f25d901f77b64c0b21e42acd6e3eddec155
Ben Sukstorf <bsukstorf@gmail.com>
2021-06-15 14:26:29 +0200

    Fix localhost:3000/users/<username> routing to PUG View

--- src/routes/users.js
+++ src/routes/users.js
@@ -95,9 +95,16 @@ router.get('/initialize', async (req, res) => {
   res.sendStatus(200)
 })
 
-router.get('/:userID', async (req, res) => {
-  const person = await Person.findById(req.params.userID)
-  if (person) res.render('user', { person })
+// router.get('/:userID', async (req, res) => {
+//   const person = await Person.findById(req.params.userID)
+//   if (person) res.render('user', { person })
+//   else res.sendStatus(404)
+// })
+
+router.get('/:name', async (req, res) => {
+  const personArray = await Person.find({ name: req.params.name })
+  const [person] = personArray
+  if (person) res.render('user', { title: `${person.name}'s Profile`, person })
   else res.sendStatus(404)
 })
 
