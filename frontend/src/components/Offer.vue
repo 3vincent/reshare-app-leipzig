ca8453ab6c28ddbc4a6d24eb954b5389e6135eb9
Ben Sukstorf <bsukstorf@gmail.com>
2021-06-21 10:32:49 +0200

    Add offer Component

---
+++ frontend/src/components/Offer.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="box">
+    <div class="photobox contentBox">
+      <img :src="offer.photos" />
+    </div>
+    <div class="contentBox">
+      <h2>{{ offer.title }}</h2>
+      <p><span class="bold">Status: </span>{{ offer.status }}</p>
+      <p class="bold">Description:</p>
+      <p>{{ offer.description }}</p>
+      <p><span class="bold">Owner: </span>{{ offer.owner[0].name }}</p>
+    </div>
+    <div class="contentBox">
+      <p>Helllo</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Offer',
+  props: ['offer'],
+  data() {
+    return {
+      default: 'not defined',
+    }
+  },
+  async created() {
+    const offersRequest = await axios.get('/api/offers')
+    this.offers = offersRequest.data
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.bold {
+  font-weight: 600;
+}
+.box {
+  background-color: white;
+  border: 1px solid green;
+  display: flex;
+  overflow: auto;
+  border-radius: 1rem;
+  margin: 22px;
+  text-align: left;
+  .contentBox {
+    float: left;
+    max-width: 33%;
+    margin: 10px 24px;
+    h2 {
+      margin-top: 0;
+    }
+  }
+  .photobox {
+    display: flex;
+    overflow: auto;
+    position: relative;
+    background: #f0f0f0;
+    border-radius: 0.3rem;
+    height: 120px;
+    width: 120px;
+    img {
+      display: block;
+      margin: auto;
+      max-height: 100px;
+      max-width: 100px;
+    }
+  }
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,30 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js åpp" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js åpp" /> -->
+    <div v-for="offer in offers" v-bind:key="offer">
+      <Offer :offer="offer" v-if="offer"></Offer>
+    </div>
+
     <div v-for="user in users" :key="user.name">{{ user.name }}, {{ user.age }}</div>
+    <div v-for="user in users" :key="user.location">
+      {{ user.name }} lives in {{ user.location[1] }} {{ user.location[0] }}
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Offer from '@/components/Offer.vue'
 import axios from 'axios'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    // HelloWorld,
+    Offer,
   },
   data() {
     return {
       users: [],
+      offers: [],
     }
   },
   async created() {
     const usersRequest = await axios.get('/api/users')
-
     this.users = usersRequest.data
+    const offersRequest = await axios.get('/api/offers')
+    this.offers = offersRequest.data
   },
 }
 </script>
