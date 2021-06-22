7cce3ec9c437c7e321e2d98710ae0357c4303e7d
Ben Sukstorf <bsukstorf@gmail.com>
2021-06-23 00:22:34 +0200

    Rename component to meet html5 specifications

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -20,7 +20,7 @@ const routes = [
   },
   {
     path: '/offers/:id',
-    name: 'Offer Detail',
+    name: 'OfferDetail',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
