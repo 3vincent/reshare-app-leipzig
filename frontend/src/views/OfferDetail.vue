d5fa84357f387bfe42b18ea224d06ffcea8cc960
Ben Sukstorf <bsukstorf@gmail.com>
2021-06-22 23:22:01 +0200

    Add Offer View in views/

---
+++ frontend/src/views/OfferDetail.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div>
+      <h1>{{ offer.title }}</h1>
+      <router-link to="/"> <span class="bold back">&#9747; </span> </router-link>
+    </div>
+    <Offer :offer="offer"></Offer>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import Offer from '@/components/Offer.vue'
+
+export default {
+  name: 'Offer Detail',
+  components: { Offer },
+  data() {
+    return {
+      offer: {},
+    }
+  },
+  async created() {
+    const offerRequest = await axios.get(`/api/offers/${this.$route.params.id}`)
+    this.offer = offerRequest.data
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.bold {
+  font-weight: 600;
+}
+.back {
+  position: absolute;
+  left: 1rem;
+  top: 1rem;
+  font-size: 2rem;
+}
+</style>
