b0749b5105c96289734eb8187c824fe537d52650
Ben Sukstorf <bsukstorf@gmail.com>
2021-06-23 22:11:30 +0200

    [UserDetails] Rename User Component to UserCard

---
+++ frontend/src/components/UserCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div>
+      <h2>fw {{ user.name }}</h2>
+      <h1>This is the user:5</h1>
+    </div>
+    <div>
+      <p>
+        <span class="bold">Location: </span
+        >{{
+          user.location
+            .map(point => point)
+            .slice()
+            .reverse()
+            .join(' ')
+        }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserCard',
+  props: ['user'],
+  data() {
+    return {
+      default: 'not defined',
+    }
+  },
+}
+</script>
+
+<style scoped lang="scss"></style>
--- frontend/src/views/UserDetail.vue
+++ frontend/src/views/UserDetail.vue
@@ -5,22 +5,19 @@
       <h1 v-else>Loading Profile Page...</h1>
       <router-link to="/"> <span class="bold back">&#9747; </span> </router-link>
     </div>
-    <User :user="user" :if="user"></User>
+    <UserCard :user="user" :if="user"></UserCard>
     <Counter></Counter>
   </div>
 </template>
 
 <script>
-import User from '@/components/User.vue'
+import UserCard from '@/components/UserCard.vue'
 import Counter from '@/components/Counter.vue'
 import { mapActions } from 'vuex'
 
 export default {
   name: 'UserDetail',
-  components: {
-    User,
-    Counter,
-  },
+  components: { UserCard, Counter },
   data() {
     return {
       user: null,
