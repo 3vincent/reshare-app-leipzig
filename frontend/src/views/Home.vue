3891c10a339eb0670df37d0ce23de76fe1b40559
Ben Sukstorf <bsukstorf@gmail.com>
2021-06-23 16:51:42 +0200

    Render simple User List on the 'home' View

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,17 +2,19 @@
   <div class="home">
     <h1>LEIPZIG SHARE APP</h1>
     <NavBar></NavBar>
+
+    <!-- User List Preview -->
+    <!-- <div v-for="user in users" :key="user.name">{{ user.name }}, {{ user.age }}</div>
+    <div v-for="user in users" :key="user.location">
+      {{ user.name }} lives in {{ user.location[1] }} {{ user.location[0] }}
+    </div> -->
+
     <div v-for="offer in offers" v-bind:key="offer._id">
       <Offer :offer="offer" v-if="offer"></Offer>
       <router-link :to="'/offers/' + offer._id">
         <Button v-if="offer"> <span class="bold">Open Offer2:</span> {{ offer.title }} </Button>
       </router-link>
     </div>
-
-    <!-- <div v-for="user in users" :key="user.name">{{ user.name }}, {{ user.age }}</div> -->
-    <!-- <div v-for="user in users" :key="user.location">
-      {{ user.name }} lives in {{ user.location[1] }} {{ user.location[0] }}
-    </div> -->
   </div>
 </template>
 
