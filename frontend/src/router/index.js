b62fb32f699146848d1061110593564f8966c509
Ben Sukstorf <bsukstorf@gmail.com>
2021-06-23 17:30:53 +0200

    [UserDetail] Add Route for User Detail Site /user/<userID>

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -26,6 +26,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "OfferDetail" */ '../views/OfferDetail.vue'),
   },
+  {
+    path: '/user/:id',
+    name: 'UserDetail',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "UserDetail" */ '../views/UserDetail.vue'),
+  },
 ]
 
 const router = new VueRouter({
