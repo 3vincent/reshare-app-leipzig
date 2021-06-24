582c0bf5720a201f10de10c1fd6a962f115217c6
Ben Sukstorf <bsukstorf@gmail.com>
2021-06-24 18:19:38 +0200

    [Fix] Update Comment Router with a sender.name query

--- backend/src/routes/comments.js
+++ backend/src/routes/comments.js
@@ -11,23 +11,10 @@ const Comment = require('../models/comment')
 router.get('/', async (req, res) => {
   const query = {}
 
-  if (req.query.status) {
-    query.status = req.query.status
+  if (req.query.sender.name) {
+    query.sender.name = req.query.sender.name
   }
 
-  if (req.query.category) {
-    query.category = req.query.category
-  }
-
-  if (req.query.location) {
-    query.location = req.query.location
-    // search in an array like this:
-    // http://leipzigsharing.localhost/api/offers?location=Berlin&location=11234
-    // express automatically iterates through the values
-    // so this is also possible:
-    // http://leipzigsharing.localhost/api/offers?location=Leipzig
-    // Source https://stackoverflow.com/a/33086861
-  }
   res.send(await Comment.find(query))
 })
 
