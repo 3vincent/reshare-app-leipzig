9067c5198a40d1616b7762594e7f32f227ab323b
Ben Sukstorf <bsukstorf@gmail.com>
2021-06-25 22:58:30 +0200

    [Account] Add registration route for new Users

--- backend/src/routes/account.js
+++ backend/src/routes/account.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const passport = require('passport')
+const Person = require('../models/person')
 
 const router = express.Router()
 
@@ -6,4 +8,15 @@ router.get('/session', (req, res) => {
   res.send(req.session)
 })
 
+router.post('/', async (req, res, next) => {
+  const { name, givenName, age, email, location, profilePhoto, password } = req.body
+
+  try {
+    const user = await Person.register({ name, givenName, age, email, location, profilePhoto }, password)
+    res.send(user)
+  } catch (e) {
+    console.log(e)
+    next(e)
+  }
+})
 module.exports = router
