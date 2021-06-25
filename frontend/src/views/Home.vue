e898a770577cc0d2d39730abea150712aeb77ac9
Ben Sukstorf <bsukstorf@gmail.com>
2021-06-25 23:02:47 +0200

    Update User List View

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -4,9 +4,9 @@
     <NavBar></NavBar>
 
     <!-- User List Preview -->
-    <div v-for="user in users" :key="user.name + '-label'">{{ user.name }}, {{ user.age }}</div>
-    <div v-for="user in users" :key="user.location + '-label'">
-      {{ user.name }} lives in {{ user.location[1] }} {{ user.location[0] }}
+    <div v-for="user in users" :key="user.name + '-label'">
+      User: <router-link :to="`/users/${user._id}`">{{ user.name }}</router-link
+      >, {{ user.age }} years old. {{ user.name }} lives in {{ user.location[1] }} {{ user.location[0] }}
     </div>
 
     <div v-for="offer in offers" v-bind:key="offer._id + '-label'">
