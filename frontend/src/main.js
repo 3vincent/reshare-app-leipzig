7af9d73e636eb4c6c58788093a45c1763d6eb740
Ben Sukstorf <bsukstorf@gmail.com>
2021-06-25 23:26:15 +0200

    [Account] Add Store (vuex) to main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,8 +8,14 @@ import './assets/css/style.css'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App),
-}).$mount('#app')
+async function main() {
+  let storeInstance = await store()
+
+  new Vue({
+    router: router(storeInstance),
+    store: storeInstance,
+    render: h => h(App),
+  }).$mount('#app')
+}
+
+main()
