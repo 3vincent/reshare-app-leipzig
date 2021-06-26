ac0ceaa0165bc6a724aa94330d3543e381c4a7ea
Ben Sukstorf <bsukstorf@gmail.com>
2021-06-27 01:51:36 +0200

    Update Frontend Layout and Eyecandy for Comments

--- frontend/src/components/CommentCard.vue
+++ frontend/src/components/CommentCard.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
     <div class="container">
-      <div class="comment">
+      <div class="comment speech-bubble">
         <!-- attention {{ comment.sender.name }} only works because of autopopulate -->
-        <p class="bold">{{ comment.sender.name }}:</p>
         <p>{{ comment.comment }}</p>
-        <p>{{ comment.creationTime }}</p>
+        <p class="dateFormat">{{ comment.creationTime }}</p>
+      </div>
+      <div class="senderName">
+        <span class="userAvatar" :style="{ backgroundImage: 'url(' + comment.sender.profilePhoto + ')' }"></span>
+        <span class="bold">{{ comment.sender.name }}</span>
       </div>
     </div>
   </div>
@@ -37,12 +40,27 @@ export default {
 // grid
 .container {
   display: grid;
-  grid-template-columns: 0.6fr 500px 0.6fr; //hacky!!!
-  grid-template-rows: 1fr;
+  grid-template-columns: 0.8fr 1.4fr 0.8fr; //hacky!!!
+  grid-template-rows: 0.2fr 0.2fr;
   gap: 0px 0px;
   grid-auto-flow: row;
-  grid-template-areas: '. comment .';
+  grid-template-areas:
+    '. comment .'
+    'senderName senderName senderName ';
 }
+@media (max-width: 600px) {
+  .container {
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr;
+    grid-template-areas:
+      'comment'
+      'senderName';
+  }
+  .comment {
+    margin: 0 10px;
+  }
+}
+
 .comment {
   grid-area: comment;
 }
@@ -51,9 +69,60 @@ export default {
 .bold {
   font-weight: 600;
 }
-div.comment {
-  background-color: rgb(212, 215, 228);
-  border-radius: 1rem;
+.container {
   margin-bottom: 2rem;
+}
+.dateFormat {
+  font-family: Helvetica, Arial, sans-serif;
+  font-weight: 600;
+  font-size: 0.8rem;
+  margin-top: 2rem;
+  margin-bottom: 0;
+}
+.senderName {
+  grid-area: senderName;
+  margin: 1rem 0;
+}
+.comment {
+  position: relative;
+  background-color: rgb(212, 215, 228);
+  margin-bottom: 2rem;
+  padding: 1rem 0;
+  font-size: 1.3rem;
+}
+
+.speech-bubble {
+  position: relative;
+  background: rgb(212, 215, 228);
+  border-radius: 1rem;
+}
+
+.speech-bubble:after {
+  content: '';
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  width: 0;
+  height: 0;
+  border: 33px solid transparent;
+  border-top-color: rgb(212, 215, 228);
+  border-bottom: 0;
+  border-left: 0;
+  margin-left: -16.5px;
+  margin-bottom: -33px;
+}
+
+.userAvatar {
+  display: inline-block;
+  position: relative;
+  margin: 0 10px;
+  top: 5px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  border-radius: 50%;
+  width: 20px;
+  height: 20px;
+  box-shadow: 0px 0px 10px 1px rgb(150, 150, 150);
 }
 </style>
