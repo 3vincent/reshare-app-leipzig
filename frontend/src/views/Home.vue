d546ccbd563eef58c39d1737fbe770d101572eeb
Ben Sukstorf <bsukstorf@gmail.com>
2021-06-27 01:53:43 +0200

    Show unique user name and real name in User List

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,7 +6,7 @@
     <!-- User List Preview -->
     <div v-for="user in users" :key="user.name + '-label'">
       User: <router-link :to="`/users/${user._id}`">{{ user.name }}</router-link
-      >, {{ user.age }} years old. {{ user.name }} lives in {{ user.location[1] }} {{ user.location[0] }}
+      >, {{ user.age }} years old. {{ user.givenName }} lives in {{ user.location[1] }} {{ user.location[0] }}
     </div>
 
     <div v-for="offer in offers" v-bind:key="offer._id + '-label'">
