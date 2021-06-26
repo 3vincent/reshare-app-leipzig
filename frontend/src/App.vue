697fe0439f4e257e174b7bd573b868dad6c65ce4
Ben Sukstorf <bsukstorf@gmail.com>
2021-06-27 01:50:58 +0200

    Remove .nav CSS-Class (moved to components)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,7 +7,6 @@
 
 <script>
 // @ is an alias to /src
-
 import Footer from '@/components/Footer.vue'
 
 export default {
@@ -19,6 +18,7 @@ export default {
 </script>
 
 <style lang="scss">
+// important: this is not scoped....
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -27,16 +27,16 @@ export default {
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+// #nav {
+//   padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
 </style>
