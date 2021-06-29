77e466f2b760b77565e3e6891ba71efd5dcc19d3
Ben Sukstorf <bsukstorf@gmail.com>
2021-06-29 17:56:35 +0200

    [Account] Add some individual variables to register view

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -3,8 +3,12 @@
     <form @submit="submitLogin">
       <h1>Create a new account</h1>
       <label for="name"
-        >Name:&nbsp;
+        >Profile: (must be unique to you)&nbsp;
         <input v-model="name" id="name" type="text" placeholder="Your name" required="required" />
+      </label>
+      <label for="givenName"
+        >Real Name: (optional)&nbsp;
+        <input v-model="givenName" id="givenName" type="text" placeholder="Your real name" />
       </label>
       <label for="age"
         >Age:&nbsp;
@@ -13,6 +17,14 @@
       <label for="email"
         >Email:&nbsp;
         <input v-model="email" id="email" type="email" placeholder="Your email" required="required" />
+      </label>
+      <label for="areaCode"
+        >Area Code:&nbsp;
+        <input v-model="areaCode" id="areaCode" type="areaCode" placeholder="Your Area Code" required="required" />
+      </label>
+      <label for="city"
+        >City:&nbsp;
+        <input v-model="city" id="city" type="city" placeholder="Your city" required="required" />
       </label>
       <label for="password"
         >Password:&nbsp;
@@ -36,7 +48,9 @@ export default {
       age: null,
       email: '',
       password: '',
-
+      city: 'Leipzig',
+      areaCode: '',
+      givenName: '',
       backendError: null,
     }
   },
@@ -48,7 +62,9 @@ export default {
       try {
         await this.register({
           name: this.name,
+          givenName: this.givenName,
           age: this.age,
+          location: [this.city, this.areaCode],
           email: this.email,
           password: this.password,
         })
