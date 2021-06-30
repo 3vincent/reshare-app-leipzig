e8aa3c3a22abd3d6714766ed8974cd83c91f92f8
Ben Sukstorf <bsukstorf@gmail.com>
2021-06-30 21:07:00 +0200

    Send test message from backend to frontend (watch in console, network tab)

--- backend/src/socket-service.js
+++ backend/src/socket-service.js
@@ -2,6 +2,10 @@ const io = require('socket.io')()
 
 io.on('connect', socket => {
   socket.emit('hello world!')
+
+  setInterval(() => {
+    socket.emit('hello world!')
+  }, 2000)
 })
 
 module.exports = io
