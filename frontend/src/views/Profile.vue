492ff447f8a50b21d7957c27baed128b0905b585
Ben Sukstorf <bsukstorf@gmail.com>
2021-07-01 16:28:09 +0200

    Add Live Chat, Broadcasting and joining Streams to Frontend View (Profile)

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -68,3 +68,36 @@ export default {
   },
 }
 </script>
+
+<template>
+  <div class="home">
+    <NavBar></NavBar>
+    <h1>{{ user.name }}</h1>
+    <h2>Leipzig Sharing ::: My User Profile</h2>
+    <h2>Users</h2>
+    <div v-for="user in users" v-bind:key="user">
+      <router-link :to="`/users/${user._id}`">{{ user.name }}</router-link>
+    </div>
+    <div v-if="liveStreams.length">
+      <h2>Live streams</h2>
+      <div v-for="stream in liveStreams" v-bind:key="stream">
+        <p>{{ stream }}</p>
+        <button @click="joinStream(stream)">Join stream</button>
+      </div>
+    </div>
+    <button @click="goLive">Go live</button>
+    <div v-if="currentLiveStream">
+      <h3>Live stream</h3>
+      <div class="messages">
+        <div class="message" v-for="message in liveStreamMessages" v-bind:key="message">
+          <p>
+            <span>{{ message.author }}:&nbsp;</span><span>{{ message.body }}</span>
+          </p>
+        </div>
+      </div>
+      <form @submit="sendMessage">
+        <input type="text" v-model="message" /><input type="submit" value="Send message" />
+      </form>
+    </div>
+  </div>
+</template>
