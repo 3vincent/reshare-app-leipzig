d5eb7c1aa5f7383d3f590c33c7d675ec349cf2aa
Ben Sukstorf <bsukstorf@gmail.com>
2021-07-01 16:25:28 +0200

    Comment out test case

--- backend/src/socket-service.js
+++ backend/src/socket-service.js
@@ -1,15 +1,16 @@
 const io = require('socket.io')()
 
 io.on('connect', socket => {
-  socket.emit('hello world!')
+  // socket.emit('hello world!')
 
-  setInterval(() => {
-    socket.emit('hello world!')
-  }, 2000)
+  // setInterval(() => {
+  //   socket.emit('hello world!')
+  // }, 2000)
 
-  socket.on('new message!', cb => {
-    console.log('a new message was received!')
-    cb('this is a response!')
+  // socket.on('new message!', cb => {
+  //   console.log('a new message was received!')
+  //   cb('this is a response!')
+  // })
   })
 })
 
