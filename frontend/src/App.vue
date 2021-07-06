2e9c820c99ff116b42b7b07e4e205ceefb43064f
Ben Sukstorf <bsukstorf@gmail.com>
2021-07-06 18:31:43 +0200

    Add Apple Mac, Apple Mobile, Windows, Linux Standard-Fonts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -20,7 +20,9 @@ export default {
 <style lang="scss">
 // important: this is not scoped....
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  // font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+    'Helvetica Neue', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
