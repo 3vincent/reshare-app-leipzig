de2c7066a6d108ffc5af7f89acf829c2883fa600
Ben Sukstorf <bsukstorf@gmail.com>
2021-07-07 18:51:56 +0200

    Import socket.io-client to frontend (vuex store)
    # Conflicts:
    #	frontend/src/store/index.js

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
+import io from 'socket.io-client'
 
 axios.defaults.baseURL = process.env.VUE_APP_BASE_URL
 axios.defaults.withCredentials = true
 
 Vue.use(Vuex)
 
-// const socket = io(process.env.VUE_APP_BASE_URL)
+const socket = io(process.env.VUE_APP_BASE_URL)
 
 const mutations = {
   INCREMENT_COUNT: 'increment count',
