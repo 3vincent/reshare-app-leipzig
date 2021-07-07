ed99ad27eae2da1dc5c0d1e9851285d762f2cc16
Ben Sukstorf <bsukstorf@gmail.com>
2021-07-07 17:42:18 +0200

    Solve rebase conflict

--- backend/src/app.js
+++ backend/src/app.js
@@ -43,6 +43,8 @@ app.set('io', socketService)
 
 app.set('trust proxy', 1)
 
+app.set('io', socketService)
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 // app.set('view engine', 'pug')
