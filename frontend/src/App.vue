074aee01609ce0445cfd89827e82ffa81df17259
Ben Sukstorf <bsukstorf@gmail.com>
2021-07-25 18:54:05 +0200

    Remove unnecessary css

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -31,17 +31,4 @@ export default {
 .bold {
   font-weight: 600;
 }
-
-// #nav {
-//   padding: 30px;
-
-//   a {
-//     font-weight: bold;
-//     color: #2c3e50;
-
-//     &.router-link-exact-active {
-//       color: #42b983;
-//     }
-//   }
-// }
 </style>
