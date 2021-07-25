ab287890c67940ead195d9316a043bf6b9b8a390
Ben Sukstorf <bsukstorf@gmail.com>
2021-07-25 18:54:24 +0200

    Change color

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -26,7 +26,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #3d3c3a;
 }
 .bold {
   font-weight: 600;
