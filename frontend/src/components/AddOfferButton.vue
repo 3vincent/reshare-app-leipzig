eb1fdee6b7183275cbbca896f128415d865f9812
Ben Sukstorf <bsukstorf@gmail.com>
2021-07-26 20:21:04 +0200

    Add CSS to make it look nicer

--- frontend/src/components/AddOfferButton.vue
+++ frontend/src/components/AddOfferButton.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="fixedContainer">
     <div class="container">
-      <div class="addSign">
-        <div class="bar horizontal"></div>
-        <div class="bar vertical"></div>
-      </div>
+      <router-link to="/addOffer">
+        <div class="addSign">
+          <div class="bar horizontal"></div>
+          <div class="bar vertical"></div>
+        </div>
+      </router-link>
     </div>
   </div>
 </template>
@@ -28,28 +30,30 @@ export default {
   border-radius: 50%;
   padding: 3px;
   border: 4px solid #4f7ca4;
-  width: 90px;
-  height: 90px;
-  margin-bottom: -40px;
-  transition: margin-bottom 0.3s ease;
-}
-.container:hover {
-  margin-bottom: 0px;
-  transition: margin-bottom 0.3s ease;
-  transition: transform 0.5s ease;
-  transform: rotate(180deg);
+  // alternative to border, from here https://stackoverflow.com/a/51496341
+  // background: linear-gradient(140deg, red, yellow, green), linear-gradient(to right, #9c20aa, #fb3570);
+  width: 100px;
+  height: 100px;
+  margin-bottom: -60px;
+  transition: margin-bottom 0.3s ease 50ms, transform 0.5s ease 0ms;
+  &:hover {
+    margin-bottom: 0px;
+    transition: margin-bottom 0.3s ease 50ms, transform 0.5s ease 0ms;
+    transform: rotate(180deg);
+    border: 4px solid #7ca44f;
+  }
 }
 .addSign {
   position: relative;
+  margin: 0 auto;
+  margin-top: 5px;
   background-color: #4f7ca4;
   width: 90px;
   height: 90px;
   border-radius: 50%;
 }
-.addSign:hover {
-  cursor: pointer;
-}
-// Plus sign CSS gratefully copied from here https://stackoverflow.com/questions/41051094/making-a-plus-sign-with-css
+
+// CSS Plus sign gratefully copied from here https://stackoverflow.com/questions/41051094/making-a-plus-sign-with-css
 .bar {
   margin: 0 auto;
   position: absolute;
